ad045d5f2bce64803cd6cd2637aa9fec0d34d9da
mhou-afk <54276044+mhou-afk@users.noreply.github.com>
2021-02-24 12:00:27 -0600

    finished bottomnavigationbar

--- lumin_lights/lib/main.dart
+++ lumin_lights/lib/main.dart
@@ -26,14 +26,12 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
@@ -43,36 +41,43 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(widget.title),
+          title: Text("AppBar"),
         ),
         body: Container(),
-        bottomNavigationBar: BottomNavigationBar(currentIndex: 0, items: [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.lightbulb),
-              // ignore: deprecated_member_use
-              title: Text('Lights'),
-              backgroundColor: Colors.blue),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              // ignore: deprecated_member_use
-              title: Text('Home'),
-              backgroundColor: Colors.blue),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              // ignore: deprecated_member_use
-              title: Text('Settings'),
-              backgroundColor: Colors.blue),
-        ]));
+        bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _currentIndex,
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.lightbulb),
+                  // ignore: deprecated_member_use
+                  title: Text('Lights'),
+                  backgroundColor: Colors.red),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  // ignore: deprecated_member_use
+                  title: Text('Home'),
+                  backgroundColor: Colors.blue),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.settings),
+                  // ignore: deprecated_member_use
+                  title: Text('Settings'),
+                  backgroundColor: Colors.yellow),
+            ],
+            onTap: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            }));
   }
 }
